e74ba65c9f867f7c3017b7cb41303e2265fefb15
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-27 23:59:06 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
-
+import { Button } from "@/components/ui/button"
 function App() {
  
 
   return (
    
-      <div>
-        <h1>Welcome to My React App</h1>
-      </div>
+     
+       
+       <div className='text-center
+       text-2xl
+       text-blue-500'>
+      <h1>Hello</h1>
+       <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+    </div>
+      
         
     
   )
