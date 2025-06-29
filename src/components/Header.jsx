e21e2d44c0cc776d800a677783da039ae5d7bcbc
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-29 23:28:27 +0530

    install dropdown menu + added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import {Button} from './ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 
 const Header = () => {
   const negavite = useNavigate()
+  const user = true;
   return (
     <nav className='items-center flex justify-between p-4 '>
       <Link to='/'>
@@ -11,7 +13,22 @@ const Header = () => {
       </Link>
 
       <div>
+      {!user?
         <Button onClick={()=>negavite('/auth')}>Login</Button>
+        :(
+          <DropdownMenu>
+  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+        )
+      }
       </div>
     </nav>
   )
