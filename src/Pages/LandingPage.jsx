c0f50d344311df4c2dbb2c4ae996ee1ea16034dd
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-07-01 22:14:14 +0530

    Landing page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -7,18 +7,31 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import { useState } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const LandingPage = () => {
+
+  const [longUrl, setLongUrl] = useState();
+  
+  const navigate = useNavigate();
+
+  const handleShorten= (e)=>{
+    e.preventDefault();
+    if(longUrl)navigate(`/auth?createNew=${longUrl}`);
+  }
   return (
     <div className='flex flex-col items-center'>
      <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-green-500 text-center font-extrabold">
         The only URL Shortener <br /> you&rsquo;ll ever need! 👇
       </h2>
-      <form  className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2">
+      <form onSubmit={handleShorten}
+       className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2">
         <Input
            type="url"
+           value={longUrl}
           placeholder="Enter your Loooong URL"
-         // onChange={}
+          onChange={(e)=>setLongUrl(e.target.value) }
           className="h-full flex-1 px-4 py-4"
         />
         <Button type="submit" className="h-full" variant="destructive">
@@ -27,14 +40,36 @@ const LandingPage = () => {
       </form>
       <img src='/banner.jpeg' alt='banner' className='w-full my-11 md:px:11'/>
 
-      <Accordion type="multiple" collapsible className='w-full  md:px-11'>
-  <AccordionItem value="item-1">
-    <AccordionTrigger>Is it accessible?</AccordionTrigger>
-    <AccordionContent>
-      Yes. It adheres to the WAI-ARIA design pattern.
-    </AccordionContent>
-  </AccordionItem>
-</Accordion>
+      <Accordion type="multiple" collapsible className="w-full md:px-11">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            How does the Trimrr URL shortener works?
+          </AccordionTrigger>
+          <AccordionContent>
+            When you enter a long URL, our system generates a shorter version of
+            that URL. This shortened URL redirects to the original long URL when
+            accessed.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>
+            Do I need an account to use the app?
+          </AccordionTrigger>
+          <AccordionContent>
+            Yes. Creating an account allows you to manage your URLs, view
+            analytics, and customize your short URLs.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>
+            What analytics are available for my shortened URLs?
+          </AccordionTrigger>
+          <AccordionContent>
+            You can view the number of clicks, geolocation data of the clicks
+            and device types (mobile/desktop) for each of your shortened URLs.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
 
     </div>
   )
