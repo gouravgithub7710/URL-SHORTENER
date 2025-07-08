0db1edd2934fca47465b5a9c5aad7cb3fc3172fd
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-07-08 23:16:52 +0530

    Auth jsx

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { useSearchParams } from 'react-router-dom'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Login from '@/components/Login';
 import Signup from '@/components/Signup';
+import {UrlState} from "@/context";
+import {useEffect} from "react";
+import {useNavigate, useSearchParams} from "react-router-dom";
 
 const Auth = () => {
   const [searchParams]=useSearchParams();
+    const longLink = searchParams.get("createNew");
+    const navigate = useNavigate();
+     const {isAuthenticated, loading} = UrlState();
+       useEffect(() => {
+    if (isAuthenticated && !loading)
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+  }, [isAuthenticated, loading, navigate]);
+
   return (
-    <div className="mt-36 flex flex-col items-center gap-10">
+    <div className="mt-10 flex flex-col items-center gap-10">
       <h1 className='text-5xl font-extrabold'>
-      {searchParams.get('createNew')
+      {longLink
            ?
       "Hold up! Let's login first..."
             :
@@ -18,8 +28,8 @@ const Auth = () => {
       </h1>
       <Tabs defaultValue="login" className="w-[400px]">
   <TabsList className="grid w-full grid-cols-2">
-    <TabsTrigger value="login">Login</TabsTrigger>
-    <TabsTrigger value="signup">Signup</TabsTrigger>
+    <TabsTrigger className='cursor-pointer' value="login">Login</TabsTrigger>
+    <TabsTrigger className='cursor-pointer' value="signup">Signup</TabsTrigger>
   </TabsList>
   <TabsContent value="login">
     <Login/>
