6769a1e4d272649c11b1fcc526a74d52797a1319
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-07-08 23:17:26 +0530

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import {Link, Navigate, useNavigate} from 'react-router-dom'
 import {Button} from './ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { LinkIcon, LogOut } from 'lucide-react'
+import { UrlState } from '@/Context'
+import { logout } from '@/DB/ApiAuth'
+import useFetch from './Hooks/use-fetch'
+import {BarLoader} from "react-spinners";
 
 const Header = () => {
-  const negavite = useNavigate()
-  const user = false;
+  const navigate = useNavigate()
+  const {user, fetchUser} = UrlState();
+  const {loading,fn:fnLogout} = useFetch(logout)
   return (
+    <>
     <nav className='items-center flex justify-between p-4 '>
       <Link to='/'>
         <img src='/logo.png' className='h-16' alt='Trimrr logo'/>
@@ -16,32 +22,40 @@ const Header = () => {
 
       <div>
       {!user?
-        <Button onClick={()=>negavite('/auth')} className='cursor-pointer'>Login</Button>
+        <Button onClick={()=>navigate('/auth')} className='cursor-pointer'>Login</Button>
         :(
           <DropdownMenu>
   <DropdownMenuTrigger>
     <Avatar>
-  <AvatarImage src="https://github.com/shadcn.png" className='cursor-pointer border-0
+  <AvatarImage src={user?.user_metadata?.profilepic} className='w-12 h-12 rounded-full object-cover cursor-pointer shadow-md hover:scale-150 transition-transform
 '/>
   <AvatarFallback>GG</AvatarFallback>
 </Avatar>
   </DropdownMenuTrigger>
   <DropdownMenuContent>
-    <DropdownMenuLabel>Gourav Giri</DropdownMenuLabel>
+    <DropdownMenuLabel>{user?.user_metadata?.name}</DropdownMenuLabel>
     <DropdownMenuSeparator />
     <DropdownMenuItem className='cursor-pointer'>
     <LinkIcon className='text-white-400'/>
     My Links</DropdownMenuItem>
     <DropdownMenuItem className="text-red-400 cursor-pointer">
     <LogOut className='text-red-400'/>
-    <span>Logout</span>
+    <span onClick={()=>{
+      fnLogout().then(()=>{
+         fetchUser();
+        navigate('/')
+      })
+      
+    }}>Logout</span>
     </DropdownMenuItem>
   </DropdownMenuContent>
 </DropdownMenu>
         )
       }
-      </div>
+      </div> 
     </nav>
+      {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
+    </>
   )
 }
 
