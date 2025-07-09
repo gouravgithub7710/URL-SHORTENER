4a5ce41e9a5d873203ff6f7b2092dea3ae69d669
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-07-10 03:34:38 +0530

    api auth

--- src/DB/ApiUrls.js
+++ src/DB/ApiUrls.js
@@ -1,4 +1,5 @@
 import supabase from "./supabase";
+import {supabaseUrl} from "./supabase";
 
 export async function getUrls(user_id) {
    const {data, error} = await supabase
@@ -11,3 +12,49 @@ export async function getUrls(user_id) {
     throw new Error('Unable to load URLs');};
   return data;
 } 
+
+export async function createUrl({title, longUrl, customUrl, user_id}, qrcode) {
+  const short_url = Math.random().toString(36).substr(2, 6);
+  const fileName = `qr-${short_url}`;
+
+  const {error: storageError} = await supabase.storage
+    .from("qrs")
+    .upload(fileName, qrcode);
+
+  if (storageError) throw new Error(storageError.message);
+
+  const qr = `${supabaseUrl}/storage/v1/object/public/qrs/${fileName}`;
+
+  const {data, error} = await supabase
+    .from("urls")
+    .insert([
+      {
+        title,
+        user_id,
+        original_url: longUrl,
+        custom_url: customUrl || null,
+        short_url,
+        qr,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Error creating short URL");
+  }
+
+  return data;
+}
+
+
+export async function deleteUrl(id) {
+  const {data, error} = await supabase.from("urls").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Unable to delete Url");
+  }
+
+  return data;
+}
