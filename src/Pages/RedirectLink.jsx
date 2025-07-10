6bbd35fcff69beb9c2aa82a79100af0f6ea75d3e
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-07-10 23:02:59 +0530

    Redirect link

--- src/Pages/RedirectLink.jsx
+++ src/Pages/RedirectLink.jsx
@@ -1,11 +1,42 @@
+import useFetch from '@/components/Hooks/use-fetch';
+import { storeClicks } from '@/DB/ApiClicks';
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import { getLongUrl } from '@/DB/ApiUrls';
+import { useEffect } from 'react';
+import BarLoader from 'react-spinners/BarLoader';
+
 
 const RedirectLink = () => {
-  return (
-    <div>
-      RedirectLink
-    </div>
-  )
-}
+  const {id} = useParams();
+    const {loading, data, fn} = useFetch(getLongUrl, id);
+
+  const {loading: loadingStats, fn: fnStats} = useFetch(storeClicks, {
+    id: data?.id,
+    originalUrl: data?.original_url,
+  });
+
+  useEffect(() => {
+    fn();
+  }, []);
+
+  useEffect(() => {
+    if (!loading && data) {
+      fnStats();
+    }
+  }, [loading]);
+
+  if (loading || loadingStats) {
+    return (
+      <>
+        <BarLoader width={"100%"} color="#36d7b7" />
+        <br />
+        Redirecting...
+      </>
+    );
+  }
+
+  return null;
+};
 
 export default RedirectLink
